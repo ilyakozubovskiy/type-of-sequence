f44bb882b9b28b111df4c8f41e5eb67124c4dbe3
ilya kozubovskiy <ilyakozubovskiy@outlook.com>
2022-01-18 09:32:37 +0300

    Type of Sequence

--- Numbers/IntegerExtensions.cs
+++ Numbers/IntegerExtensions.cs
@@ -14,7 +14,50 @@ namespace Numbers
         /// or null if the information is not defined.</returns>
         public static ComparisonSigns? GetTypeComparisonSigns(this long number)
         {
-            throw new NotImplementedException();
+            ComparisonSigns comparisonSigns = 0;
+
+            if (number == long.MinValue)
+            {
+                number = long.MaxValue;
+            }
+
+            int length = 1;
+            long tmp = number;
+
+            while ((tmp /= 10) >= 1)
+            {
+                length++;
+            }
+
+            long[] digitArray = new long[length];
+            for (int i = 1; number > 0; number /= 10, i++)
+            {
+                digitArray[^i] = number % 10;
+            }
+
+            for (int i = 0; i < digitArray.Length - 1; i++)
+            {
+                long nextDigit = digitArray[i + 1];
+                switch (digitArray[i])
+                {   
+                    case long digit when digit < nextDigit:
+                        comparisonSigns |= ComparisonSigns.LessThan;
+                        break;
+                    case long digit when digit > nextDigit:
+                        comparisonSigns |= ComparisonSigns.MoreThan;
+                        break;
+                    case long digit when digit == nextDigit:
+                        comparisonSigns |= ComparisonSigns.Equals;
+                        break;
+                }
+            }
+
+            if (comparisonSigns > 0)
+            {
+                return comparisonSigns;
+            }
+
+            return null;
         }
 
         /// <summary>
@@ -24,7 +67,18 @@ namespace Numbers
         /// <returns>The information in the form of a string about the type of sequence that the digit of a given number represents.</returns>
         public static string GetTypeOfDigitsSequence(this long number)
         {
-            throw new NotImplementedException();
+            return GetTypeComparisonSigns(number) switch
+            {
+                null => "One digit number.",
+                ComparisonSigns.LessThan => "Strictly Increasing.",
+                ComparisonSigns.MoreThan => "Strictly Decreasing.",
+                ComparisonSigns.Equals => "Monotonous.",
+                ComparisonSigns.LessThan | ComparisonSigns.Equals => "Increasing.",
+                ComparisonSigns.MoreThan | ComparisonSigns.Equals => "Decreasing.",
+                ComparisonSigns.LessThan | ComparisonSigns.MoreThan => "Unordered.",
+                ComparisonSigns.LessThan | ComparisonSigns.MoreThan | ComparisonSigns.Equals => "Unordered.",
+                _ => null
+            };
         }
     }
 }
